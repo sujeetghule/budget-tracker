386b075ec12e540673bf36981052b8e9cbfa5569
sujeetghule <sujeetghule8@gmail.com>
2022-08-20 10:59:10 +0530

    final

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,37 +1,63 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
+import { v4 as uuidv4 } from 'uuid';
 
-const AddExpenseForm = () => {
+const AddExpenseForm = (props) => {
+	const { dispatch } = useContext(AppContext);
+
+	const [name, setName] = useState('');
+	const [cost, setCost] = useState('');
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		const expense = {
+			id: uuidv4(),
+			name,
+			cost: parseInt(cost),
+		};
+
+		dispatch({
+			type: 'ADD_EXPENSE',
+			payload: expense,
+		});
+
+		setName('');
+		setCost('');
+	};
 
 	return (
-		<form>
-			<div className='row'>
-				<div className='col-sm'>
+		<form onSubmit={onSubmit}>
+			<div class='row'>
+				<div class='col-sm col-lg-4'>
 					<label for='name'>Name</label>
 					<input
 						required='required'
 						type='text'
-						className='form-control'
+						class='form-control'
 						id='name'
-					></input>
+						value={name}
+						onChange={(event) => setName(event.target.value)}
+					/>
 				</div>
-				<div className='col-sm'>
+				<div class='col-sm col-lg-4'>
 					<label for='cost'>Cost</label>
 					<input
 						required='required'
-						type='text'
-						className='form-control'
+						type='number'
+						class='form-control'
 						id='cost'
-					></input>
+						value={cost}
+						onChange={(event) => setCost(event.target.value)}
+					/>
 				</div>
-
 			</div>
-            <div className='row'>
-            	<div className='col-sm'>
-					<button type='submit' className='btn btn-primary mt-3'>
+			<div class='row mt-3'>
+				<div class='col-sm'>
+					<button type='submit' class='btn btn-primary'>
 						Save
 					</button>
 				</div>
-            </div>
+			</div>
 		</form>
 	);
 };
